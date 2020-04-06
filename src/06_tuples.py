13e98083519ebf0cb0eaab86b60aa0f5d46e47e0
nathan-saygers <nathan.saygers@gmail.com>
2020-04-06 15:01:53 -0700

    minor correction in 06

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -45,5 +45,5 @@ print_tuple(t)
 
 
 # Declare a tuple of 1 element then print it
-u = ('1')  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
