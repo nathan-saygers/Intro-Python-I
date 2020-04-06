442482f56e8e227e8d44ff04337c95ca4771efb4
nathan-saygers <nathan.saygers@gmail.com>
2020-04-06 14:22:28 -0700

    04 and 05 complete

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,40 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+
+x.append(4)
+print('pass number 1', x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+x.extend(y)
+print('pass number 2', x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+x.pop(4)
+print('pass number 3', x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+
+x.insert(5, 99)
+print('pass number 4', x)
 
 # Print the length of list x
 # YOUR CODE HERE
 
+print('length of x:', len(x))
+
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+final = []
+
+for num in x:
+  num = num * 1000
+  final.append(num)
+
+print('final pass:', final)
